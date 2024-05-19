282412981fd64c20d34c4d9025b51bd4ce751678
andresbermeoq <andresbermeoq1@gmail.com>
2024-05-19 00:29:46 -0500

    feat: add nvim lsp configuration languages.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,4 +1,11 @@
 local lsp_zero = require('lsp-zero')
+local lspconfig_status_ok, lpsconfig = pcall(function ()
+	return require('lspconfig')
+end)
+
+if not lspconfig_status_ok then
+	return
+end
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -6,12 +13,13 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
- handlers = { 
-	 function (server_name)
-		 require('lspconfig')[server_name].setup({})
-	 end,
- },
-
+	ensure_installed = {'dockerls', 'pyright', 'rust_analyzer', 'tsserver', 'volar', 'lua_ls'},
+	handlers = {
+		function(server_name)
+			lpsconfig[server_name].setup({})
+		end,
+	},
 })
--- nvim/lua/andresdev/packer.lua
+++ nvim/lua/andresdev/packer.lua
@@ -12,13 +12,14 @@ return require('packer').startup(function(use)
   }
   -- Treesiter
   use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+  -- LSP-Zero
   use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v3.x',
   requires = {
     --- Uncomment the two plugins below if you want to manage the language servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/nvim-cmp'},
