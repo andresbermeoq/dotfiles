eb4bcc6dcec7917339ec3b0d5dc2264359fb1951
andresbermeoq <andresbermeoq1@gmail.com>
2024-05-21 13:19:44 -0500

    feat: Add status line and configure it.

---
+++ nvim/after/plugin/lualine.lua
@@ -0,0 +1,38 @@
+local status_ok, lualine = pcall(function()
+  return require("lualine")
+end)
+
+if not status_ok then
+  return
+end
+
+local function attached_clients()
+  return "(" .. vim.tbl_count(vim.lsp.buf_get_clients(0)) .. ")"
+end
+
+local function cwd()
+  return vim.fn.fnamemodify(vim.loop.cwd(), ":~")
+end
+
+lualine.setup({
+  options = {
+    globalstatus = true,
+  },
+  sections = {
+    lualine_b = { "branch", "diff", cwd },
+    lualine_c = { { "filename", file_status = true, path = 1 } },
+    lualine_x = {
+      { "diagnostics", sources = { "nvim_diagnostic" } },
+      "filesize",
+      "encoding",
+      { "filetype", separator = { right = "" }, right_padding = 0 },
+      { attached_clients, separator = { left = "" }, left_padding = 0 },
+    },
+  },
+  winbar = {
+    lualine_c = { "filename" },
+  },
+  extensions = {
+    "fugitive",
+  },
+})
--- nvim/lua/andresdev/packer.lua
+++ nvim/lua/andresdev/packer.lua
@@ -56,4 +56,8 @@ return require("packer").startup(function(use)
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
   })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true },
+  })
 end)
