97325432420496c27c4883a872729dd9a8302a78
andresbermeoq <andresbermeoq1@gmail.com>
2024-05-21 16:11:08 -0500

    fix: Add conventional commits
    Add conventional commits in the telescope, separate the logic in a function and create a keymap.
    
    Keymap, telescope, Conventional_commits

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -9,10 +9,33 @@ local builtin = require("telescope.builtin")
 if not status_ok then
   return
 end
+
+local function create_conventional_commit()
+  local actions = require("telescope._extensions.conventional_commits.actions")
+  local picker = require("telescope._extensions.conventional_commits.picker")
+  local themes = require("telescope.themes")
+
+  -- if you use the picker directly you have to provide your theme manually
+  picker({
+    action = actions.prompt,
+    include_body_and_footer = true,
+    -- theme = themes["get_ivy"]() -- ivy theme
+  })
+end
 -- Keymaps Telescope
 keymap.set("n", "<C-p>", builtin.find_files, {})
 keymap.set("n", "<C-g>", builtin.git_status, {})
 keymap.set("n", "<C-r>", builtin.buffers, {})
 keymap.set("n", "<C-b>", builtin.git_branches, {})
+keymap.set("n", "<C-s>", create_conventional_commit, { noremap = true, silent = true, desc = "Conventional Commits" })
 
-telescope.setup({})
+telescope.setup({
+  extensions = {
+    conventional_commits = {
+      theme = "dropdown",
+      include_body_and_footer = false,
+    },
+  },
+})
+
+telescope.load_extension("conventional_commits")
